573026727c045a4e7fd5a64f940683a48dc5516f
Harish Sathisan <harish.sathisan@icloud.com>
2022-03-11 14:43:48 -0700

    Adding code to display All Product Titles

--- client/src/components/ProductForm.js
+++ client/src/components/ProductForm.js
@@ -1,10 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 export const ProductForm = () => {
 	const [Title, setTitle] = useState("");
 	const [Price, setPrice] = useState("");
 	const [Description, setDescription] = useState("");
+
+	const [listAllProducts, setListAllProducts] = useState("");
+
+	useEffect(() => {
+		axios
+			.get("http://localhost:8000/api/listAllProducts")
+			.then((res) => {
+				console.log(res.data);
+				setListAllProducts(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, [listAllProducts]);
 
 	const onCreateHandler = (e) => {
 		e.preventDefault();
@@ -21,51 +35,57 @@ export const ProductForm = () => {
 		setDescription("");
 	};
 	return (
-		<div>
-			<form onSubmit={onCreateHandler}>
-				<div className="main">
-					<div className="fashion">
-						<p>
-							<label>Title</label>
-							<input
-								type="text"
-								className="entrytxt2"
-								value={Title}
-								onChange={(e) => setTitle(e.target.value)}
-							/>
-						</p>
-					</div>
-					<br />
-					<div className="fashion">
-						<p>
-							<label>Price</label>
-							<input
-								type="text"
-								className="entrytxt2"
-								value={Price}
-								onChange={(e) => setPrice(e.target.value)}
-							/>
-						</p>
-					</div>{" "}
-					<br />
-					<div className="fashion">
-						<p>
-							<label>Description</label>
-							<input
-								type="text"
-								className="entrytxt"
-								value={Description}
-								onChange={(e) => setDescription(e.target.value)}
-							/>
-						</p>
-					</div>{" "}
-					<br />
-					<input type="submit" value="Create Product" className="submit" />
+		<form onSubmit={onCreateHandler}>
+			<div className="main">
+				<div className="fashion">
+					<p>
+						<label>Title</label>
+						<input
+							type="text"
+							className="entrytxt2"
+							value={Title}
+							onChange={(e) => setTitle(e.target.value)}
+						/>
+					</p>
 				</div>
-			</form>
-			<div>
-				<hr></hr>
+				<br />
+				<div className="fashion">
+					<p>
+						<label>Price</label>
+						<input
+							type="text"
+							className="entrytxt2"
+							value={Price}
+							onChange={(e) => setPrice(e.target.value)}
+						/>
+					</p>
+				</div>{" "}
+				<br />
+				<div className="fashion">
+					<p>
+						<label>Description</label>
+						<input
+							type="text"
+							className="entrytxt"
+							value={Description}
+							onChange={(e) => setDescription(e.target.value)}
+						/>
+					</p>
+				</div>{" "}
+				<br />
+				<input type="submit" value="Create Product" className="submit" />
+				<div>
+					<br />
+					<hr></hr>
+					<div>
+						<p>ProductName:</p>
+
+						{listAllProducts.map((listAll, index) => (
+							<li key={index}>{listAll.Title}</li>
+						))}
+					</div>
+				</div>
 			</div>
-		</div>
+		</form>
 	);
 };
--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -1,6 +1,6 @@
 const productManagerController = require("../controllers/ProductManager.controller");
 
 module.exports = (app) => {
-	app.get("/api", productManagerController.showAll);
+	app.get("/api/listAllProducts", productManagerController.showAll);
 	app.post("/api/product", productManagerController.createProduct);
 };
