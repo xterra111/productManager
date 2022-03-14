9aa9b1cdf7240ad62c951fc1fe6fc54fd03004b6
Harish Sathisan <harish.sathisan@icloud.com>
2022-03-13 21:34:21 -0700

    Del btns + func to del item in DB & Displ all list

--- client/src/components/DisplayAll.js
+++ client/src/components/DisplayAll.js
@@ -4,6 +4,7 @@ import { Link, useParams } from "react-router-dom";
 
 const DisplayAll = (props) => {
 	const { listAllProducts, setListAllProducts } = props;
+	//const { id } = useParams();
 	useEffect(() => {
 		axios
 			.get("http://localhost:8000/api/listAllProducts")
@@ -15,12 +16,34 @@ const DisplayAll = (props) => {
 				console.log(err);
 			});
 	}, []);
+
+	const onDeleteHandler = (idFromBelow) => {
+		//e.preventDefault();
+		axios
+			.delete(`http://localhost:8000/api/${idFromBelow}`)
+			.then((res) => {
+				console.log(res.data);
+				setListAllProducts(
+					listAllProducts.filter(
+						(listAll, index) => listAll._id !== idFromBelow
+					)
+				);
+			})
+
+			.catch((err) => console.log(err));
+	};
+
 	return (
 		<div>
 			<h1>All Available Products</h1>
 			{listAllProducts.map((listAll, index) => (
 				<div key={index}>
-					<Link to={`/${listAll._id}`}>{listAll.Title}</Link>
+					<div>
+						<Link to={`/${listAll._id}`}>{listAll.Title}</Link>
+					</div>
+					<div>
+						<button onClick={() => onDeleteHandler(listAll._id)}>Delete</button>
+					</div>
 				</div>
 			))}
 		</div>
